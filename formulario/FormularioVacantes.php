36e6cbbc4a15b5896bcb7f92f03769bb989bfe0d
Annibel <2190007@ipisa.edu.do>
2022-11-03 20:06:45 -0400

    Se creo formulario de vacantes

---
+++ formulario/FormularioVacantes.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VACANTES</title>
+</head>
+<body>
+    <P>Nombre de la empresa:</P>
+    <p>Nombre del puesto:</p>
+    <p>Funciones o perfil del puesto:</p>
+    <p>Sueldo:</p>
+    <p>Ubicación:</p>
+    <p>Tipo de contrato:</p>
+    <p></p>
+    
+    
+</body>
+</html>
